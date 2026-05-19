b50e1f80c33b3ca5b069ec15cb2d307f9260f688
shobhitnegi <128126291+shobhitnegi@users.noreply.github.com>
2026-05-19 15:05:12 +0000

    datatypes

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 13000;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "12345";
+accountCity = "GZB"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// accountId = 1
+accountEmail = "THG"
+accountPassword = "56"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
